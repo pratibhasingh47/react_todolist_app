66ac3dc0a38658df5d9de532f71609d016f72606
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-06-18 22:18:33 +0530

    .js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,6 +7,7 @@ export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
 	return (
 		<div className='Todo'>
 			<p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed' : ""}`}>{task.task}</p>
+			
 			<div>
 				<FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)} />
 				<FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
